651df5949aeb07ac4d39ce3030bb1c466d2231ef
Zhengshunda <daishunda@outlook.com>
2024-03-26 09:32:08 +0800

    spi erro

---
+++ light_LPN/build/image_preload.cmake
@@ -0,0 +1,8 @@
+# Generated file that can be used to preload variant images
+set(BOARD "nrf52840dk_nrf52840" CACHE INTERNAL "NCS child image controlled")
+set(BOARD_ROOT "d:/bluemesh/tflite/tflite-v2/mesh_2.4.2;d:/bluemesh/tflite/tflite-v2/light_switch_lpn;d:/bluemesh/tflite/tflite-v2/light_lpn;D:/ncs/v2.4.2/v2.4.2/nrf;D:/ncs/v2.4.2/v2.4.2/zephyr" CACHE INTERNAL "NCS child image controlled")
+set(CACHED_CONF_FILE "D:/bluemesh/tflite/tflite-v2/light_LPN/prj.conf" CACHE INTERNAL "NCS child image controlled")
+set(CONF_FILE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
+set(DTC_OVERLAY_FILE "" CACHE INTERNAL "NCS child image controlled")
+set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
+set(WEST_PYTHON "C:/ncs/toolchains/31f4403e35/opt/bin/python.exe" CACHE INTERNAL "NCS child image controlled")
---
+++ light_switch_LPN/build/image_preload.cmake
@@ -0,0 +1,9 @@
+# Generated file that can be used to preload variant images
+set(BOARD "nrf52840dk_nrf52840" CACHE INTERNAL "NCS child image controlled")
+set(BOARD_ROOT "d:/bluemesh/tflite/tflite-v2/mesh_2.4.2;d:/bluemesh/tflite/tflite-v2/light_switch_lpn;d:/bluemesh/tflite/tflite-v2/light_lpn;d:/bluemesh/tflite/tflite-v2/light_switch;D:/ncs/v2.4.2/v2.4.2/nrf;D:/ncs/v2.4.2/v2.4.2/zephyr" CACHE INTERNAL "NCS child image controlled")
+set(CACHED_CONF_FILE "d:/bluemesh/tflite/tflite-v2/light_switch_LPN/prj.conf" CACHE INTERNAL "NCS child image controlled")
+set(CONF_FILE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
+set(DTC_OVERLAY_FILE "D:/bluemesh/tflite/tflite-v2/light_switch_LPN/nrf52840dk_nrf52840.overlay" CACHE INTERNAL "NCS child image controlled")
+set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
+set(OVERLAY_CONFIG "d:/bluemesh/tflite/tflite-v2/light_switch_LPN/overlay-lpn.conf" CACHE INTERNAL "NCS child image controlled")
+set(WEST_PYTHON "C:/ncs/toolchains/31f4403e35/opt/bin/python.exe" CACHE INTERNAL "NCS child image controlled")
